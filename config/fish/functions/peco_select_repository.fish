cee6ba0a1a7af3d86146f3a4399437c6b1a78bcb
Cside <cside.story@gmail.com>
2018-03-10 11:19:31 +0900

    sort by mtime desc

--- config/fish/functions/peco_select_repository.fish
+++ config/fish/functions/peco_select_repository.fish
@@ -1,7 +1,7 @@
 function peco_select_repository
     if set -q $argv
-        ghq list -p | peco | read line; builtin cd $line
+        ghq list -p | sort-by-mtime | peco | read line; builtin cd $line
     else
-        ghq list -p | peco --query $argv | read line; builtin cd $line
+        ghq list -p | sort-by-mtime | peco --query $argv | read line; builtin cd $line
     end
 end
