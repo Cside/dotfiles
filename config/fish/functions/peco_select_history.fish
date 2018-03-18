0d9d4940e107b1f896d31ae8116249e974784ba9
Cside <cside.story@gmail.com>
2018-03-18 16:52:49 +0900

    missing scrit

--- config/fish/functions/peco_select_history.fish
+++ config/fish/functions/peco_select_history.fish
@@ -1,1 +1,15 @@
-/Users/cside/.config/fisherman/peco/functions/peco_select_history.fish
+function peco_select_history
+  if test (count $argv) = 0
+    set peco_flags
+  else
+    set peco_flags--query "$argv"
+  end
+
+  history|peco $peco_flags|read foo
+
+  if [ $foo ]
+    commandline $foo
+  else
+    commandline ''
+  end
+end
